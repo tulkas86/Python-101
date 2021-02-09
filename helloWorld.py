f937a146099cd8da6abf42cbf921803388084e12
Luis Vanni <31259997+tulkas86@users.noreply.github.com>
2021-02-09 14:52:52 -0300

    Update helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,8 +1,9 @@
-print("Develop Versión 2.0")
-print("En esta versión del software realizaremos cuentas más complejas")
-print("Ingrese el primer valor a sumar: ")
+print("Feature Versión 2.0.1")
+#En esta versión del software realizaremos cuentas más complejas#
+name = input("Bienvenido, ¿como es tu nombre? ")
+print(name + " Ingrese el primer valor a sumar: ")
 num1 = input()
 print("Ingrese el segundo valor a sumar: ")
 num2 = input()
 total = int(num1) + int(num2)
-print("El valor de la suma es: " + str(total))
+print("Bueno " + name + " El valor de la suma es: " + str(total))
